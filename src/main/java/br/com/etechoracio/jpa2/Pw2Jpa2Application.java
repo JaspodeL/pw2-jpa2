0435bc6cbaeb17f6e6f71885dac9a99c5765e0ce
Rafael-Ysabela <Rafael-Ysabela@test.com>
2022-09-19 10:41:10 -0300

    Criacao do projeto

---
+++ src/main/java/br/com/etechoracio/jpa2/Pw2Jpa2Application.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.jpa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pw2Jpa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pw2Jpa2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/etechoracio/jpa2/Pw2Jpa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.jpa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw2Jpa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
